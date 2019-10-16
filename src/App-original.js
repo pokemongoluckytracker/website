24e6ddec22584ad494d02805e1a5690b778ae007
Jenny Cooke <Jenny.Cooke@bottomline.com>
2019-10-16 09:18:46 +0100

    Changing logic

---
+++ src/App-original.js
@@ -0,0 +1,105 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Pokemon from './pokemon-released.json';
+
+function App() {
+  const pokeArray = Object.values(Pokemon);
+  const checkedShiny = JSON.parse(localStorage.getItem('shinyPokemon'));
+  const checkedLucky = JSON.parse(localStorage.getItem('luckyPokemon'));
+  const checkedPurified = JSON.parse(localStorage.getItem('purifiedPokemon'));
+  let shinyChecked, luckyChecked, purifiedChecked  = false;
+
+  const pokemonList = pokeArray.map(({id, name}) => {
+
+ const checkedItem = checkedShiny.find(entry => entry === Number(id));
+ console.log('checkedItem', checkedItem);
+
+    if(checkedItem === Number(id)){
+      shinyChecked = "checked";  
+    }
+
+    return (
+      <React.Fragment key={id}>
+        <tr>
+          <td>{id}</td>
+          <td>{name}</td>
+          <td>
+            <input 
+              type="checkbox"
+              checked={shinyChecked}
+              onChange={() => { checkboxChange('shiny', {id}) }}
+            ></input>
+          </td>
+          <td>
+            <input 
+              type="checkbox" 
+              checked={luckyChecked} 
+              onChange={() => { checkboxChange('lucky', {id}) }}
+            ></input>
+          </td>
+          <td>
+            <input 
+              type="checkbox" 
+              checked={purifiedChecked} 
+              onChange={() => { checkboxChange('purified', {id}) }}
+            ></input>
+          </td>
+        </tr>
+      </React.Fragment>
+    )
+  });
+  
+  function checkboxChange(type, id) {
+    if(type === 'shiny') {
+      let checkedShiny = JSON.parse(localStorage.getItem('shinyPokemon')) || [];
+      checkedShiny.push(
+        id = id.id
+      );
+
+    const checkedShinyList = JSON.stringify(checkedShiny);
+    localStorage.setItem('shinyPokemon', checkedShinyList);
+    }
+
+    if(type === 'lucky') {
+      let checkedLucky = JSON.parse(localStorage.getItem('luckyPokemon')) || [];
+      checkedLucky.push(
+        id = id.id
+      );
+
+    const checkedLuckyList = JSON.stringify(checkedLucky);
+    localStorage.setItem('luckyPokemon', checkedLuckyList);
+    }
+
+    if(type === 'purified') {
+      let checkedPurified = JSON.parse(localStorage.getItem('purifiedPokemon')) || [];
+      checkedPurified.push(
+        id = id.id
+      );
+
+    const checkedPurifiedList = JSON.stringify(checkedPurified);
+    localStorage.setItem('purifiedPokemon', checkedPurifiedList);
+    }
+    
+  };
+
+  return (
+    <React.Fragment>
+      <table className="pokesLabTable">
+        <thead>
+          <tr>
+            <th>Number</th>
+            <th>Name</th>
+            <th>Shiny</th>
+            <th>Lucky</th>
+            <th>Purified</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemonList}
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -4,96 +4,54 @@ import Pokemon from './pokemon-released.json';
 
 function App() {
   const pokeArray = Object.values(Pokemon);
-  const checkedShiny = JSON.parse(localStorage.getItem('shinyPokemon'));
-  const checkedLucky = JSON.parse(localStorage.getItem('luckyPokemon'));
-  const checkedPurified = JSON.parse(localStorage.getItem('purifiedPokemon'));
-  let shinyChecked, luckyChecked, purifiedChecked  = false;
+
+  const userPokemon = new Array(pokeArray[pokeArray.length - 1].id + 1).fill().map(() => ({}));
+
+  const userLocalStorage = JSON.parse(localStorage.getItem('userPokemon'));
+
+  if (Array.isArray(userLocalStorage)) {
+    userLocalStorage.forEach((entry, index) => userPokemon[index] = entry);
+  }
+
+  /**
+   * Update the `userPokemon` array with the new state of the changed pokemon
+   * @param {Number} id - The ID of the Pokemon.
+   * @param {String} type - The update context.
+   * @param {Boolean} isChecked - Whether the context is marked as true or false for the Pokemon.
+   */
+  function updatePokemon(id, type, isChecked) {
+    userPokemon[id][type] = isChecked;
+    localStorage.setItem('userPokemon', JSON.stringify(userPokemon));
+  }
+
 
   const pokemonList = pokeArray.map(({id, name}) => {
-    if(Number(checkedShiny) === Number(id)){
-      shinyChecked = true;
-    }
-    else {
-      shinyChecked = false;
-    }
-
-    if(Number(checkedLucky) === Number(id)){
-      luckyChecked = true;
-    }
-    else {
-      luckyChecked = false;
-    }
     
-    if(Number(checkedPurified) === Number(id)){
-      purifiedChecked = true;
-    }
-    else {
-      purifiedChecked = false;
-    }
-
     return (
-      <React.Fragment key={id}>
-        <tr>
-          <td>{id}</td>
-          <td>{name}</td>
-          <td>
-            <input 
-              type="checkbox"
-              checked={shinyChecked}
-              onChange={() => { checkboxChange('shiny', {id}) }}
-            ></input>
-          </td>
-          <td>
-            <input 
-              type="checkbox" 
-              checked={luckyChecked} 
-              onChange={() => { checkboxChange('lucky', {id}) }}
-            ></input>
-          </td>
-          <td>
-            <input 
-              type="checkbox" 
-              checked={purifiedChecked} 
-              onChange={() => { checkboxChange('purified', {id}) }}
-            ></input>
-          </td>
-        </tr>
-      </React.Fragment>
+      <tr key={id}>
+        <td>{id}</td>
+        <td>{name}</td>
+        <td>
+          <input 
+            type="checkbox"
+            onChange={(event) => updatePokemon(id, 'shiny', event.currentTarget.checked)}
+          />
+        </td>
+        <td>
+          <input 
+            type="checkbox"
+            onChange={(event) => updatePokemon(id, 'lucky', event.currentTarget.checked)}
+          />
+        </td>
+        <td>
+          <input 
+            type="checkbox"
+            onChange={(event) => updatePokemon(id, 'purified', event.currentTarget.checked)}
+          />
+        </td>
+      </tr>
     )
   });
-  
-  function checkboxChange(type, id) {
-    if(type === 'shiny') {
-      let checkedShiny = JSON.parse(localStorage.getItem('shinyPokemon')) || [];
-      checkedShiny.push(
-        id = id.id
-      );
-
-    const checkedShinyList = JSON.stringify(checkedShiny);
-    localStorage.setItem('shinyPokemon', checkedShinyList);
-    }
-
-    if(type === 'lucky') {
-      let checkedLucky = JSON.parse(localStorage.getItem('luckyPokemon')) || [];
-      checkedLucky.push(
-        id = id.id
-      );
-
-    const checkedLuckyList = JSON.stringify(checkedLucky);
-    localStorage.setItem('luckyPokemon', checkedLuckyList);
-    }
-
-    if(type === 'purified') {
-      let checkedPurified = JSON.parse(localStorage.getItem('purifiedPokemon')) || [];
-      checkedPurified.push(
-        id = id.id
-      );
-
-    const checkedPurifiedList = JSON.stringify(checkedPurified);
-    localStorage.setItem('purifiedPokemon', checkedPurifiedList);
-    }
-    
-  };
 
   return (
     <React.Fragment>
